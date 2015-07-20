53de5f5ef74a947defd484a1bb8ffc3a708124f7
SkyLothar <allothar@gmail.com>
2015-07-20 12:36:25 +0800

    decode base64 encoded email address

--- src/vendor/js/a-hair-salon.js
+++ src/vendor/js/a-hair-salon.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-   var bodyHeight = $("body").height()-$(window).height();
-   window.onscroll = function() {
+  // transform base64 encoded email address to normal text
+  // we use base64ed email address, so simple email-spiders can go fuck themself
+  var b64Email = $("#email").html();
+  $("#email").html(atob(b64Email));
 
-      //Determine the amount to rotate by.
-      var deg = -window.scrollY*(360/bodyHeight);
+  var bodyHeight = $("body").height() - $(window).height();
+  window.onscroll = function() {
+    // Determine the amount to rotate by.
+    var deg = -window.scrollY * (360 / bodyHeight);
 
-      $(".square").css({
-        "transform": "rotate("+deg+"deg)",
-      });
-
-   };
+    $(".square").css({
+      "transform": "rotate(" + deg + "deg)",
+    });
+  };
 });
