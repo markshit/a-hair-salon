91605dbfadde3f54b40c1a10c20acdf017a73ac0
SkyLothar <allothar@gmail.com>
2015-07-20 12:38:34 +0800

    Updates

--- js/a-hair-salon.js
+++ js/a-hair-salon.js
@@ -1,28 +1,16 @@
-(function() {
-  var b64email;
+$(document).ready(function(){
+  // transform base64 encoded email address to normal text
+  // we use base64ed email address, so simple email-spiders can go fuck themself
+  var b64Email = $("#email").html();
+  $("#email").html(atob(b64Email));
 
-  b64email = $("#email").html();
+  var bodyHeight = $("body").height() - $(window).height();
+  window.onscroll = function() {
+    // Determine the amount to rotate by.
+    var deg = -window.scrollY * (360 / bodyHeight);
 
-  $("#email").html(atob(b64email));
-
-  $(".showbox").flip({
-    trigger: "manual"
-  });
-
-  $(".showbox").flip(false);
-
-  $(".show-arrow").click(function() {
-    var idx, j, ref, results, showboxes;
-    window.location = "#what-i-had-done-before";
-    $(".row").removeClass("show-arrow");
-    showboxes = $(".showbox");
-    results = [];
-    for (idx = j = 0, ref = showboxes.length; 0 <= ref ? j <= ref : j >= ref; idx = 0 <= ref ? ++j : --j) {
-      results.push(setTimeout(function(i) {
-        return $($(".showbox")[i]).flip(true);
-      }, idx * 100 + 100, idx));
-    }
-    return results;
-  });
-
-}).call(this);
+    $(".square").css({
+      "transform": "rotate(" + deg + "deg)",
+    });
+  };
+});
